94709c98ef1ed7564712314efee41ab0265adf3f
24 <24@DESKTOP-2RICABM>
2020-03-21 13:55:42 +0900

    Common URL & Postman Test

--- example/src/main/java/com/example/controller/MainController.java
+++ example/src/main/java/com/example/controller/MainController.java
@@ -1,14 +1,30 @@
 package com.example.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.common.Url;
 
 @Controller
 public class MainController {
 	
-	@GetMapping(value = {"/"})
+	@GetMapping(value = {Url.MAIN.MAIN})
 	public String index() {
+		
 		System.out.println("성공");
+		
 		return "main";
+	}
+	
+	@PostMapping(value = {Url.POSTMAN.PARAM})
+	public String test(@RequestParam HashMap<String, Object> param) {
+		
+		System.out.println(param.get("param"));
+		
+		return "123";
 	}
 }
